e481f1aefd68e59f83c95ee24cf1625474a366ed
YATENDRA AWANA <yka6739@gmail.com>
2021-09-26 16:23:59 +0530

    Add project files.

---
+++ WebApplication29/Client/Repository/GenreRepository.cs
@@ -0,0 +1,30 @@
+﻿
+using WebApplication29.Shared.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Form1.Client.Helpers;
+
+namespace WebApplication29.Client.Repository
+{
+    public class GenreRepository : IGenreRepository
+    {
+        private readonly IHttpService httpService;
+        private string url = "api/genres";
+
+        public GenreRepository(IHttpService httpService)
+        {
+            this.httpService = httpService;
+        }
+        public async Task CreateGenre(Online genre)
+        {
+            var response = await httpService.Post(url, genre);
+            if (!response.Success)
+            {
+                throw new ApplicationException(await response.GetBody());
+            }
+        }
+    }
+}
---
+++ WebApplication29/Client/Repository/IGenreRepository.cs
@@ -0,0 +1,14 @@
+﻿using WebApplication29.Shared.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace WebApplication29.Client.Repository
+{
+    public interface IGenreRepository
+    {
+        Task CreateGenre(Online genre);
+    }
+}
